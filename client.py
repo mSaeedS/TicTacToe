d4dba3c4470a94a1c1035c5d4791d17b649c64a7
mSaeedS <msaeed.saleem678@gmail.com>
2024-05-13 21:36:34 +0500

    xd

--- client.py
+++ client.py
@@ -2,35 +2,38 @@ import pygame
 import socket
 import time
 import threading
-import pickle
 
 s = socket.socket()
 host = "localhost"
 port = 9999
 
 playerOne = 1
+playerOneColor = (255, 0, 0)
 playerTwo = 2
-
-allow = 0 # allow handling mouse events
+playerTwoColor = (0, 0, 255)
+bottomMsg = ""
+msg = "Waiting for peer"
+currentPlayer = 0
 xy = (-1, -1)
+allow = 0 #allow handling mouse events
+matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-# Create worker threads
+#Create worker threads
 def create_thread(target):
-    t = threading.Thread(target=target) # argument - target function
+    t = threading.Thread(target = target) #argument - target function
     t.daemon = True
     t.start()
 
-# Initialize
+#initialize
 pygame.init()
 
 width = 600
 height = 550
 screen = pygame.display.set_mode((width, height))
 
-# Set title
-pygame.display.set_caption("Tic Tac Toe")
 
-# Fonts
+
+#fonts
 bigfont = pygame.font.Font('freesansbold.ttf', 64)
 smallfont = pygame.font.Font('freesansbold.ttf', 32)
 backgroundColor = (255, 255, 255)
@@ -38,29 +41,103 @@ titleColor = (0, 0, 0)
 subtitleColor = (128, 0, 255)
 lineColor = (0, 0, 0)
 
-def buildScreen(bottomMsg, string, playerColor=subtitleColor):
-    # your UI building logic
-    pass
+def buildScreen(bottomMsg, string, playerColor = subtitleColor):
+    screen.fill(backgroundColor)
+    if "One" in string or "1" in string:
+        playerColor = playerOneColor
+    elif "Two" in string or "2" in string:
+        playerColor = playerTwoColor
 
+    #vertical lines
+    pygame.draw.line(screen, lineColor, (250-2, 150), (250-2, 450), 4)
+    pygame.draw.line(screen, lineColor, (350-2, 150), (350-2, 450), 4)
+    #horizontal lines
+    pygame.draw.line(screen, lineColor, (150, 250-2), (450, 250-2), 4)
+    pygame.draw.line(screen, lineColor, (150, 350-2), (450, 350-2), 4)
+
+
+    subtitle = smallfont.render(str.upper(string), True, playerColor)
+    screen.blit(subtitle, (150, 70))
+    centerMessage(bottomMsg, playerColor)
+
+def centerMessage(msg, color = titleColor):
+    pos = (100, 480)
+    # screen.fill(backgroundColor)
+    if "One" in msg or "1" in msg:
+        color = playerOneColor
+    elif "Two" in msg or "2" in msg:
+        color = playerTwoColor
+    msgRendered = smallfont.render(msg, True, color)
+    screen.blit(msgRendered, pos)
+
+def printCurrent(current, pos, color):
+    currentRendered = bigfont.render(str.upper(current), True, color)
+    screen.blit(currentRendered, pos)
+
+def printMatrix(matrix):
+    for i in range(3):
+        #When row increases, y changes
+        y = int((i + 1.75) * 100) 
+        for j in range(3):
+            #When col increases, x changes
+            x =  int((j + 1.75) * 100)
+            current = " "
+            color = titleColor
+            if matrix[i][j] == playerOne:
+                current = "X"
+                color = playerOneColor
+            elif matrix[i][j] == playerTwo:
+                current = "O"
+                color = playerTwoColor
+            printCurrent(current, (x, y), color)
+
+#5 Checks is x,y that are generation by handlemouseEvent are within 3x3 matrix of tictactoe
+def validate_input(x, y):
+    if x > 3 or y > 3:
+        print("\nOut of bound! Enter again...\n")
+        return False
+    elif matrix[x][y] != 0:
+        print("\nAlready entered! Try again...\n")
+        return False
+    return True
+
+#4 Receives pos from start_game and determines if position is valid or invalid, if its valid then matrix for client is updated.
 def handleMouseEvent(pos):
-    # your mouse event handling logic
-    pass
+    x = pos[0]
+    y = pos[1]
+    global currentPlayer
+    global xy
+    if(x < 150 or x > 450 or y < 150 or y > 450):
+        xy = (-1, -1)
+    else:
+        
+        col = int(x/100 - 1.5)
+        
+        row = int(y/100 - 1.5)
+        print("({}, {})".format(row,col))
+        if validate_input(row, col):
+            matrix[row][col] = currentPlayer
+            xy = (row,col)
 
+#1 Client first connects to the host and port defined by server, then receives input from server of what player the client is
 def start_player():
+    global currentPlayer
+    global bottomMsg
     try:
         s.connect((host, port))
-        print("Connected to:", host, ":", port)
+        print("Connected to :", host, ":", port)
         recvData = s.recv(2048 * 10)
         bottomMsg = recvData.decode()
         if "1" in bottomMsg:
             currentPlayer = 1
         else:
-            currentPlayer = 2
+            currentPlayer =2
         start_game()
         s.close()
     except socket.error as e:
         print("Socket connection error:", e) 
 
+#2 Thread is created to receive message from server while the loop runs, takes input via mouse
 def start_game():
     running = True
     global msg
@@ -80,9 +157,15 @@ def start_game():
             break
         
         buildScreen(bottomMsg, msg)                      
+        printMatrix(matrix) 
         pygame.display.update()
 
+#3 Separate thread is created for this function to take input from server
+#When input the client indicating that the client should input coordinates for their move,  It then enters a loop to wait for valid coordinates (xy) from the user. 
+#On matrix it receives updated matrix from server and updates the local matrix
+#On Over it recieves the last message indicating that game is over
 def accept_msg():
+    global matrix
     global msg
     global bottomMsg 
     global allow
@@ -100,26 +183,27 @@ def accept_msg():
                 while failed:
                     try:
                         if xy != (-1, -1):
-                            coordinates = str(xy[0]) + "," + str(xy[1])
+                            coordinates = str(xy[0])+"," + str(xy[1])
                             s.send(coordinates.encode())
                             failed = 0
                             allow = 0
                     except:
-                        print("Error occurred....Try again")
+                        print("Error occured....Try again")
 
             elif recvDataDecode == "Error":
-                print("Error occurred! Try again..")
+                print("Error occured! Try again..")
             
             elif recvDataDecode == "Matrix":
+                print(recvDataDecode)
                 matrixRecv = s.recv(2048 * 100)
-                matrix = pickle.loads(matrixRecv)
-                print(matrix)  # This is for debugging, you'll integrate this data into your UI
+                matrixRecvDecoded = matrixRecv.decode("utf-8")
+                matrix = eval(matrixRecvDecoded)
 
             elif recvDataDecode == "Over":
                 msgRecv = s.recv(2048 * 100)
-                msg = msgRecv.decode("utf-8")
-                bottomMsg = msg
-                msg = "~~~Game Over~~~"
+                msgRecvDecoded = msgRecv.decode("utf-8")
+                bottomMsg = msgRecvDecoded
+                msg = "Game Over"
                 break
             else:
                 msg = recvDataDecode
@@ -130,7 +214,7 @@ def accept_msg():
             break
 
         except:
-            print("Error occurred")
+            print("Error occured")
             break
 
 start_player()
--- server.py
+++ server.py
@@ -1,38 +1,88 @@
 import socket
 import pickle
 import time
-import classes
 
+#Created socket and defined host,port,matrix
 s = socket.socket()
 host = "localhost"
 port = 9999
+matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
+#2 players list for connection and address storage
 playerOne = 1
 playerTwo = 2
-
 playerConn = list()
 playerAddr = list()       
 
-game = classes.TicTacToe()
+#4 It's checks for player and then notes down its connection, it then received input from client in row,column form and decods it into integer, to add values to matrix
+#If player1 then matrix value=1, if player 2 then matrix value=2
+def get_input(currentPlayer):
+    if currentPlayer == playerOne:
+        player = "Player 1 Turn"
+        conn = playerConn[0]
+    else:
+        player = "Player 2 Turn"
+        conn = playerConn[1]
+    print(player)
+    send_common_msg(player)
+    try:
+        #Sends both client input to receive their next move
+        conn.send("Input".encode())
+        data = conn.recv(2048 * 10)
+        conn.settimeout(20)
+        dataDecoded = data.decode().split(",")
+        x = int(dataDecoded[0])
+        y = int(dataDecoded[1])
+        matrix[x][y] = currentPlayer
+        #Sends updated matrix to both client
+        send_common_msg("Matrix")
+        send_common_msg(str(matrix))
+    except:
+        conn.send("Error".encode())
+        print("Error occured! Try again..")
 
-def get_input(currentPlayer, conn):
-    while True:
-        try:
-            conn.send("Input".encode())
-            data = conn.recv(2048 * 10)
-            conn.settimeout(20)
-            dataDecoded = data.decode().split(",")
-            x = int(dataDecoded[0])
-            y = int(dataDecoded[1])
-            if game.make_move(x, y, currentPlayer):
+def check_rows():
+    
+    result = 0
+    for i in range(3):
+        if matrix[i][0] == matrix[i][1] and matrix[i][1] == matrix[i][2]:
+            result = matrix[i][0]
+            if result != 0:
                 break
-            else:
-                conn.send("Error".encode())
-                print("Error occurred! Try again..")
-        except:
-            print("Error occurred! Try again..")
+    return result
 
+def check_columns():
+    
+    result = 0
+    for i in range(3):
+        if matrix[0][i] == matrix[1][i] and matrix[1][i] == matrix[2][i]:
+            result = matrix[0][i]
+            if result != 0:
+                break
+    return result
+
+def check_diagonals():
+    
+    result = 0
+    if matrix[0][0] == matrix[1][1] and matrix[1][1] == matrix[2][2]:
+        result = matrix[0][0]
+    elif matrix[0][2] == matrix[1][1] and matrix[1][1] == matrix[2][0]:
+        result = matrix[0][2]
+    return result
+
+#5 Checks if a player has won by rows,column or diagonal otherwise draw
+def check_winner():
+    result = 0
+    result = check_rows()
+    if result == 0:
+        result = check_columns()
+    if result == 0:
+        result = check_diagonals()
+    return result
+
+#1 The host and port are binded, it can only accept 2 players
 def start_server():
+    
     try:
         s.bind((host, port))
         print("Tic Tac Toe server started \nBinding to port", port)
@@ -41,11 +91,13 @@ def start_server():
     except socket.error as e:
         print("Server binding error:", e)
     
+
+#2 In iteration of 2 players, connection and address are stored to the list using s.accept()
 def accept_players():
     try:
         for i in range(2):
             conn, addr = s.accept()
-            msg = "<<< You are player {} >>>".format(i+1)
+            msg = "You are player {}".format(i+1)
             conn.send(msg.encode())
 
             playerConn.append(conn)
@@ -57,27 +109,29 @@ def accept_players():
     except socket.error as e:
         print("Player connection error", e)
     except KeyboardInterrupt:
-        print("\nKeyboard Interrupt")
-        exit()
+            print("\nKeyboard Interrupt")
+            exit()
     except Exception as e:
         print("Error occurred:", e)
 
+#3 Until result changes or 9 iterations are complete loop will get input from player and check for winner
 def start_game():
     result = 0
     i = 0
     while result == 0 and i < 9 :
         if (i%2 == 0):
-            get_input(playerOne, playerConn[0])
+            get_input(playerOne)
         else:
-            get_input(playerTwo, playerConn[1])
-        result = game.check_win()
+            get_input(playerTwo)
+        result = check_winner()
         i = i + 1
-    
+        
+    #Sends message to both clients to inform that game is over
     send_common_msg("Over")
 
     if result == 1:
         lastmsg = "Player One is the winner!!"
-    elif result == -1:
+    elif result == 2:
         lastmsg = "Player Two is the winner!!"
     else:
         lastmsg = "Draw game!! Try again later!"
@@ -87,10 +141,10 @@ def start_game():
     for conn in playerConn:
         conn.close()
     
-
+#Sends message to connection of both players
 def send_common_msg(text):
-    for conn in playerConn:
-        conn.send(text.encode())
-        time.sleep(1)
+    playerConn[0].send(text.encode())
+    playerConn[1].send(text.encode())
+    time.sleep(1)
 
 start_server()
